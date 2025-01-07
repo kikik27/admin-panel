2a6a338232d202cff0cc27ef062cba208d2369e6
kikik27 <mkikik27@gmail.com>
2025-01-07 22:17:16 +0700

    Add Customer and Delivery resources with CRUD functionality

---
+++ app/Filament/Resources/DeliveryResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DeliveryResource\Pages;
+use App\Filament\Resources\DeliveryResource\RelationManagers;
+use App\Models\Delivery;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DeliveryResource extends Resource
+{
+    protected static ?string $model = Delivery::class;
+
+    protected static ?string $navigationGroup = 'Master Data';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make("name")->required()->unique("deliveries"),
+                Forms\Components\Toggle::make("is_active")->required()->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('Nama'),
+                Tables\Columns\ToggleColumn::make('is_active')->label('Status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDeliveries::route('/'),
+            'create' => Pages\CreateDelivery::route('/create'),
+            'edit' => Pages\EditDelivery::route('/{record}/edit'),
+        ];
+    }
+}
