b66bfdbdb8305b59f7fc87b4a543e2df144966ec
kikik27 <mkikik27@gmail.com>
2025-01-20 00:47:52 +0700

    Refactor middleware usage syntax for EncryptApiResponse in app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use(middleware: EncryptApiResponse::class);
+        $middleware->use([EncryptApiResponse::class]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
